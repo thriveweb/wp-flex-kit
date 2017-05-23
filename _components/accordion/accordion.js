83e31211b02d6e6ad53e395e8669483f77b6ee09
Vincent Weltje <vincent@thriveweb.com.au>
2017-05-24 09:05:48 +1000

    removed unused javascript from accordion component

--- _components/accordion/accordion.js
+++ _components/accordion/accordion.js
@@ -1,12 +1,7 @@
 (function _accordion($) {
   $('.accordion .accordion-item h4').on('click', function() {
     $(this).closest('.accordion').find('.accordion-item h4 svg #vertical').stop().fadeIn().closest('.accordion-item').find('> div').stop().slideUp()
-    if (!$(this).find('+ div').is(':visible')) {
-      $(this).find('svg #vertical').stop().fadeOut()
-    }
+    if (!$(this).find('+ div').is(':visible')) $(this).find('svg #vertical').stop().fadeOut()
     $(this).find('+ div').stop().slideToggle()
   })
-  if ($('.accordion').data('show-first') === true) {
-    $('.accordion .accordion-item:first-of-type h4').trigger('click')
-  }
 })(jQuery)
