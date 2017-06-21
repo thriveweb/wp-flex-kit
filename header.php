5899f9c7ab4a1eb4baf749e139c49066c8d7ee50
Vincent Weltje <vincent@thriveweb.com.au>
2017-06-22 09:58:12 +1000

    updated header with new component assets loader

--- header.php
+++ header.php
@@ -1,17 +1,22 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS" href="<?php bloginfo('rss2_url'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0" />
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
 
 	<?php
-	new Component('simple_header');
-	new Component('edit_link');
+	Component::create_assets();
+	wp_head();
 	?>
 
+</head>
+
+<body <?php body_class() ?>>
+
+	<?php new Component('simple_header') ?>
+
 	<div class="content-wrap">
+
+		<?php get_template_part('inc-edit') ?>
