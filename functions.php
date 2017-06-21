baa2f6e8cd3ecc1569fea3f6dbafabe019480135
Vincent Weltje <vincent@thriveweb.com.au>
2017-06-22 09:55:34 +1000

    added define ENVIRONMENT to functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+session_start();
+
+switch ($_SERVER['HTTP_HOST']) {
+  case file_exists(str_replace('public/', '', ABSPATH) . 'vagrantfile') :
+  case in_array($_SERVER['REMOTE_ADDR'], array( 'localhost', '127.0.0.1', '::1' )) :
+  case preg_match('/.dev/') :
+  define('ENVIRONMENT', 'development');
+  break; case preg_match('/.thrivex.io/') :
+  define('ENVIRONMENT', 'testing');
+  break; default:
+  define('ENVIRONMENT', 'production');
+  break;
+}
+
 require_once('_functions/_cp_loader.php');
 require_once('_functions/_cp_functions.php');
 
@@ -13,5 +27,3 @@ require_once('_functions/acf.php');
 // require_once('_functions/yoast.php');
 // require_once('_functions/cpt.php');
 require_once('_functions/pagination.php');
-
-?>
