2d0dfee30b83810c877228212a89170641bd273a
Vincent Weltje <vincent@thriveweb.com.au>
2017-06-22 09:59:32 +1000

    added clear cache button to frontend.

--- inc-edit.php
+++ inc-edit.php
@@ -4,6 +4,10 @@ if (is_user_logged_in()) {
   global $current_user;
   wp_get_current_user();
   edit_post_link('<i class="fa fa-pencil" aria-hidden="true"></i> <span>Edit Page</span>', '<p class="edit_link">', '</p>');
-}
 
-?>
+  if ($current_user->ID === 1) {
+    $selector = uniqid();
+    $_SESSION['renew_cp_cache_uri_selector'] = $selector;
+    echo '<p class="cp_cache"><a href="' . get_permalink() . '?' . $selector . '=' . time() . '"><i class="fa fa-hdd-o" aria-hidden="true"></i> <span>Renew CP Cache</span></a></p>';
+  }
+}
