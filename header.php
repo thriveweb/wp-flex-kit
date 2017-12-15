252a039e5c0ec4e0b236b697adfe6c7c23b68c57
Alex F <Firthir@users.noreply.github.com>
2017-12-15 15:02:52 +1000

    Add seo variations link to header

--- header.php
+++ header.php
@@ -6,6 +6,9 @@
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS" href="<?php bloginfo('rss2_url'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0" />
 
+	<meta name="google-site-verification" content="_dvJbwcrL6miDaxPd3oYM1hH7Vw38oStzKxFatxx2sQ" />
+	<meta name="msvalidate.01" content="B0112812CA622A13C327AF572E2BA5B5" />
+
 	<?php
 	Component::create_assets();
 	wp_head();
